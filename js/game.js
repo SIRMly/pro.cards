b9e1dbb92afa3fade458c79fa198fbc76456f245
SIRMly <429625819@qq.com>
2017-06-05 16:03:53 +0800

    ##

--- js/game.js
+++ js/game.js
@@ -8,39 +8,22 @@ $(function(){
     //    e.preventDefault();
     //};
     var imgs = [
-        "img/0.png",
-        "img/1.png",
-        "img/2.png",
-        "img/3.png",
-        "img/4.png",
-        "img/5.png",
-        "img/6.png",
-        "img/7.png",
-        "img/again-btn.png",
-        "img/concern-btn.png",
-        "img/continue-btn.png",
-        "img/crash-bg.png",
-        "img/fail-flag.png",
-        "img/head.png",
-        "img/home-btn.png",
-        "img/page1-bg.jpg",
-        "img/page1-btn.png",
-        "img/page1-notice.png",
-        "img/page1-word.png",
-        "img/pic.png",
-        "img/rank-btn.png",
-        "img/reward-btn.png",
-        "img/state.png",
-        "img/success-flag.png",
-        "img/timer-bg.png"
+        "img/0.jpg",
+        "img/1.jpg",
+        "img/2.jpg",
+        "img/3.jpg",
+        "img/4.jpg",
+        "img/5.jpg",
+        "img/6.jpg",
+        "img/7.jpg",
+        "img/8.jpg",
+        "img/9.jpg"
     ];
     var proBox = $("#progress");
-    var pages = $(".page");
-    var gamePage = $("#game-center");
-
     preload(imgs,proBox);
     function preload(imgs,proBox){
-        var proNum   = 0,
+        var progress = 0,
+            proNum   = 0,
             count    = imgs.length;
         for(var i in imgs){
             var newImg = new Image();
@@ -48,24 +31,20 @@ $(function(){
             console.log( newImg.src);
             newImg.onload = function (){
                 proNum++;
+                progress = Math.floor(proNum/count)*100;
                 if(proNum>=count){
+                    progress = 100;
                     proBox.addClass("hide");
-                    $(".page1").removeClass("hide");
+                    $("#container").removeClass("hide");
                 }
+                proBox.html(progress+"%");
             }
         }
     }
-    var tab = $(".tab"),
-        tabShow = $(".tab-show");
-    var startBtn = $("#page1-btn"),
-        againBtn = $(".again-btns"),
-        explainBtn  = $(".explain-btn"),
-        rewardBtn = $("#reward-btn"),
-        homeBtn = $("#home-btn"),
-        explainClose  = $("#close"),
-        page1Explain = $("#page1-notice"),
-        page1Close = $("#close-page1-btn");
-
+    var startBtn = $("#start-btn");
+    var againBtn = $("#again-btn");
+    var rankBtn  = $("#rank-btn");
+    var rankClose  = $("#rank-close");
     startBtn.on(click, function () {
         game.init();
     });
@@ -74,50 +53,20 @@ $(function(){
         $(".card").removeClass("cardRotateBack");
         game.init();
     });
-    explainBtn.on(click, function (){
-       $("#explain").removeClass("hide");
+    rankBtn.on("click", function (){
+       $("#rank").removeClass("hide");
     });
-    explainClose.on(click, function (){
-       $("#explain").addClass("hide");
+    rankClose.on("click", function (){
+       $("#rank").addClass("hide");
     });
-    page1Explain.on(click, function (){
-       $("#page1-state").removeClass("hide");
-    });
-    page1Close.on(click, function (){
-       $("#page1-state").addClass("hide");
-    });
-    rewardBtn.on(click, function (){
-        $("#success").addClass("hide");
-        $("#fail-img").attr({
-            "src":"img/reward1.png",
-            "width" : "100%"
-        });
-        $("#fail").removeClass("hide");
-    });
-    tab.each(function (index,item){
-        $(this).on(click, function (){
-            tabShow.eq(index).siblings(".tab-show").addClass("hide");
-            tabShow.eq(index).removeClass("hide");
-        });
-    });
-    homeBtn.on(click, function (){
-        pages.addClass("hide");
-        $("#crash,#crashes>div").addClass("hide");
-        $(".page1").removeClass("hide");
-    });
-
-
-
     var game = {
         init : function () {
             $("#crashes,#crashes>div").addClass("hide");
-            pages.addClass("hide");
-            gamePage.removeClass("hide");
             this.pics.removeClass("trans");
             this.pics.off(click);
             this.eachNum = 0;
             this.successNum = 0;
-            this.time = 300;
+            this.time = 600;
             this.timeText = "60.0s";
             this.gameOn();
             this.timeBox.html(this.timeText);
@@ -126,7 +75,7 @@ $(function(){
         doubleNum : 8,
         cards : $(".card"),
         pics : $(".pic"),
-        timeBox : $(".title-time"),
+        timeBox : $("#title-time"),
         failArr: [],
         gameOn : function (){
             /*==图片数组==*/var that = this;
@@ -140,26 +89,27 @@ $(function(){
             /*==添加图片==*/
             for(var i=0; i<cardArray.length; i++){
                 this.cards.eq(i).css({
-                    "background" : "url('img/"+ cardArray[i] +".png') center center  no-repeat ",
+                    "background" : "url('img/"+ cardArray[i] +".jpg') center center  no-repeat ",
                     "background-size" : "contain"
                 });
                 this.pics.eq(i).attr("data-num",cardArray[i]);
             }
             /*==图片添加完成==*/
-            //$("#crashes,#crashes>div").addClass("hide");
+            $("#crashes,#crashes>div").addClass("hide");
             $("#game-center").removeClass("hide");
+
             this.cards.addClass("cardRotate");
             this.pics.addClass("trans");
             setTimeout(function (){
                 game.pics.removeClass("trans");
                 game.play();
             },3000);
+
         },
         numberRandom : function (){
             return Math.random() > 0.5 ? 1 :-1;
         },
         play : function (){
-            /*==计时==*/
             this.timeStart();
             /*==绑定点击函数==*/
             $.each(this.pics, function (){
@@ -187,8 +137,10 @@ $(function(){
                             if(game.successNum==game.doubleNum){
                                 clearInterval(game.timer);
                                 setTimeout(function (){
-                                    $("#crashes,#success").removeClass("hide");
-                                    $("#yourTime").text((600-game.time)/10+"s");
+                                    $("#game-center").addClass("hide");
+                                    $("#crashes,#restart-crash").removeClass("hide");
+                                    $("#success").text("挑战成功!");
+                                    $("#using-time").text("用时： "+((600-game.time)/10)+"s");
                                 },500);
                             }
                         }else{
@@ -209,11 +161,10 @@ $(function(){
                     game.timeBox.html(game.gameText);
                 } else{
                     clearInterval(game.timer);
-                    $("#fail-img").attr({
-                        "src":"img/fail-img.png",
-                        "width" : "80%"
-                    });
-                    $("#crashes,#fail").removeClass("hide");
+                    $("#game-center").addClass("hide");
+                    $("#success").text("挑战失败!");
+                    $("#using-time").text("已超时！");
+                    $("#crashes,#restart-crash").removeClass("hide");
                 }
             },100)
         }
